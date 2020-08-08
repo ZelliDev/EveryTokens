dc0b9c909cdb9aaf574ed7cd9ae58b6763482d8a
ZelliDev <zellifone@gmail.com>
2020-08-08 09:08:30 -0300

    Initial Upload
    Initial Version Of EveryTokens

---
+++ EveryTokens/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiscordPortal.Net;
+
+namespace EveryTokens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "EveryToken";
+            PortalClient client = new PortalClient();
+            string token = File.ReadAllText("token.txt");
+
+            // Turn to True to save tokens
+            bool SaveToken = false;
+            client.Connect(token);
+
+            Applications app = client.GetApplications();
+            Bot bot = app.Apps[0].bot;
+            string tokenlist = "";
+            foreach (Apps ap in app.Apps)
+            {
+                
+                if (ap.bot != null)
+                {
+                    Console.WriteLine(ap.bot.username+"#"+ap.bot.discriminator.ToString()+" | "+ap.bot.token);
+                    tokenlist += bot.token + Environment.NewLine;
+                }
+            }
+            if (SaveToken)
+            {
+                var c = File.Create("tokenlist.txt");
+                c.Dispose();
+                c.Close();
+                File.WriteAllText("tokenlist.txt",tokenlist);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
